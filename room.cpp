58414652ddc424128e1087235a0d2510cc157774
ninja2002a <52730345+ninja2002a@users.noreply.github.com>
2019-12-16 10:47:39 -0800

    Add files via upload

---
+++ room.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include "room.h"
+#include "zuul.cpp"
+
+using namespace std;
+
+room::room() {
+  roomList = new room*[30];
+}
+
+  void roomSky() {
+    int cloud;
+  }
---
+++ room.h
@@ -0,0 +1,12 @@
+#ifndef ROOM_H
+#define ROOM_H
+#include <cstring>
+
+class room {
+ public:
+  room** roomList;
+  int cloud;
+  room();
+};
+
+#endif
